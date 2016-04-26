b41f863fab7fe71402833fb8a02678029cc0538b
Gatt <zacharygatt@me.com>
2016-04-27 09:46:34 +1000

    Fix #3

--- src/space/gatt/JavacordCommander/JavacordCommander.java
+++ src/space/gatt/JavacordCommander/JavacordCommander.java
@@ -144,7 +144,8 @@ public class JavacordCommander {
 		MessageBuilder builder = new MessageBuilder();
 		builder.append("```" + Settings.getHelpMessageLanguage()).appendNewLine();
 		for (String group : helpLines.keySet()){
-			builder.append(Settings.getHelpMessageBreaker().replace("%group", group)).appendNewLine();
+			builder.append(Settings.getHelpMessageBreaker().replace("%group", group));
+			builder.appendNewLine();
 			for (String msg : helpLines.get(group)){
 				builder.append(msg).appendNewLine();
 			}
