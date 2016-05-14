7ba77029f87b79ee9e3a3cd0081f5ea12b8dc5ef
Gatt <zacharygatt@me.com>
2016-05-14 12:15:17 +1000

    14/May
    Moved Annotation classes to their own folder
    Added a MessageManager to asynchronously send and delete messages,
    which allows the bypassing of the 5 messages per second limit.

--- src/space/gatt/JavacordCommander/annotations/Aliases.java
+++ src/space/gatt/JavacordCommander/annotations/Aliases.java
@@ -1,4 +1,4 @@
-package space.gatt.JavacordCommander;
+package space.gatt.JavacordCommander.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -11,9 +11,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE) //can use in method only.
-public @interface CommandSettings {
-	String[] value() default "";
-	boolean deleteInitatingMsg() default false;
-	boolean sendResponseViaPM() default false;
-	boolean requiresPM() default false;
+public @interface Aliases {
+	String[] value();
 }
